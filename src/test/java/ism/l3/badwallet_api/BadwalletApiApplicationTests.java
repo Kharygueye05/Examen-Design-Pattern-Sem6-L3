23b94ef276c0f20a17dce72c634f9302b2cb47fc
Mame Khary Gueye <mamakharygueye4@gmail.com>
2026-06-29 10:49:37 +0000

    chore: initialisation du projet

---
+++ src/test/java/ism/l3/badwallet_api/BadwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ism.l3.badwallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
